01f3dfc8e2766b6ecd3d190a1204db97a235a6c2
austincunningham <austincunningham@hotmail.com>
2016-09-12 22:54:39 +0100

    welcome view added

---
+++ app/src/main/java/app/donation/Welcome.java
@@ -0,0 +1,17 @@
+package app.donation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by austin on 12/09/2016.
+ */
+public class Welcome extends AppCompatActivity{
+
+    public void loginButtonPressed (View view)
+    {
+        Log.v("Welcome", "Login Pressed!");
+    }
+}
